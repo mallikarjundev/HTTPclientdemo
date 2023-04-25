646cb642a37d1eb7dee60133b869d1995e87edd2
arjunoggu <mallikarjundev2@gmail.com>
2023-04-24 21:37:48 -0400

    updated post method

--- src/com/httpclient/demo/HttpClientSyncDemo.java
+++ src/com/httpclient/demo/HttpClientSyncDemo.java
@@ -11,6 +11,14 @@ public class HttpClientSyncDemo {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(new URI("https://fakerestapi.azurewebsites.net//api/v1/Activities")).GET().build();
+
+        request = HttpRequest.newBuilder(new URI("https://fakerestapi.azurewebsites.net//api/v1/Activities")).POST(HttpRequest.BodyPublishers.ofString("{\n" +
+                "  \"id\": 0,\n" +
+                "  \"title\": \"string\",\n" +
+                "  \"dueDate\": \"2023-04-25T01:29:06.141Z\",\n" +
+                "  \"completed\": true\n" +
+                "}")).header("Content-Type","application/json").build();
+
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("status==> "+ response.statusCode());
         System.out.println(response.body());
